a25e3ca1b8c9707563c7b1b4e154e8b0f906bce4
Zack Michener <zack@zackmichener.net>
2013-05-31 09:33:45 -0700

    add basic internal object support

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -10,12 +10,15 @@ function Game() {
       , canvas
       , ctx
       , frameRate = 30
-      , gameLoop;
+      , gameLoop
+      , ship;
 
     canvasEl = $("canvas#wormhole");
     canvasEl.attr("width", width);
     canvasEl.attr("height", height);
     canvas = canvasEl.get(0).getContext("2d");
+
+    ship = new Ship();
 
     function update() {
 
@@ -24,7 +27,7 @@ function Game() {
     function draw() {
         canvas.clearRect(0, 0, width, height);
         canvas.fillStyle = "#000";
-        canvas.fillText("Hey!", 50, 50);
+        ship.draw();
     }
 
     /** public members/methods */
@@ -47,5 +50,27 @@ function Game() {
 
     };
 
+    /** internal objects */
+    function Ship(I) {
+        I = I || {};
+        
+        var _Ship = {
+            "x": I.x || width / 2,
+            "y": I.y || height  / 2,
+
+            "update": function() {
+
+            },
+
+            "draw": function() {
+                canvas.fillText("Hey!", this.x, this.y);
+            },
+
+        };
+
+        return _Ship;
+    }
+
     return _Game;
 }
+
