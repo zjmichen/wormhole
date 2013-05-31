3628c1c9d5ef76f69863d81eadb65e561dc620a6
Zack Michener <zack@zackmichener.net>
2013-05-31 10:31:04 -0700

    add movement and key handling

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -11,23 +11,64 @@ function Game() {
       , ctx
       , frameRate = 30
       , gameLoop
-      , ship;
+      , ships = []
+      , keystatus = {};
 
     canvasEl = $("canvas#wormhole");
     canvasEl.attr("width", width);
     canvasEl.attr("height", height);
     canvas = canvasEl.get(0).getContext("2d");
 
-    ship = new Ship();
+    ships.push(new Ship());
+    ships.push(new Ship({
+        "color": "red",
+        "x": 50,
+        "y": 40,
+        "angle": 3,
+    }));
+
+    $(document).bind("keydown", "left", function() {
+        keystatus.left = true;
+    });
+    $(document).bind("keyup", "left", function() {
+        keystatus.left = false;
+    });
+    $(document).bind("keydown", "right", function() {
+        keystatus.right = true;
+    });
+    $(document).bind("keyup", "right", function() {
+        keystatus.right = false;
+    });
+    $(document).bind("keydown", "up", function() {
+        keystatus.up = true;
+        return false;
+    });
+    $(document).bind("keyup", "up", function() {
+        keystatus.up = false;
+    });
 
     function update() {
+        if (keystatus.left) {
+            ships[0].turnLeft();
+        }
+        if (keystatus.right) {
+            ships[0].turnRight();
+        }
+        if (keystatus.up) {
+            ships[0].accelerate();
+        }
 
+        ships.forEach(function(ship) {
+            ship.update();
+        });
     }
 
     function draw() {
         canvas.clearRect(0, 0, width, height);
-        canvas.fillStyle = "#000";
-        ship.draw();
+
+        ships.forEach(function(ship) {
+            ship.draw();
+        });
     }
 
     /** public members/methods */
@@ -53,17 +94,46 @@ function Game() {
     /** internal objects */
     function Ship(I) {
         I = I || {};
-        
+
+        var drag = 0.9;
+
         var _Ship = {
             "x": I.x || width / 2,
             "y": I.y || height  / 2,
+            "width": I.width || 50,
+            "height": I.height || 50,
+            "angle": I.angle || 2,
+            "color": I.color || "#00f",
+            "speed": I.speed || 0,
 
             "update": function() {
-
+                this.speed *= drag;
+                this.x += this.speed*Math.cos(this.angle);
+                this.y += this.speed*Math.sin(this.angle);
             },
 
             "draw": function() {
-                canvas.fillText("Hey!", this.x, this.y);
+                canvas.save();
+                canvas.translate(this.x, this.y);
+                canvas.rotate(this.angle);
+                canvas.translate(-0.5*this.width, -0.5*this.height);
+
+                canvas.fillStyle = this.color;
+                canvas.fillRect(0, 0, this.width, this.height);
+
+                canvas.restore();
+            },
+
+            "turnLeft": function() {
+                this.angle -= 0.1;
+            },
+
+            "turnRight": function() {
+                this.angle += 0.1;
+            },
+
+            "accelerate": function() {
+                this.speed += 1;
             },
 
         };
