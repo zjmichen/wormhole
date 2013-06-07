428e09f3bf56987f5f5da2012bf9cc3296f9eb65
Zack Michener <zack@zackmichener.net>
2013-06-06 17:13:02 -0700

    add wormhole favicon

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var express = require('express')
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
