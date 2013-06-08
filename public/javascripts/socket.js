11271c5be7e25930a2f9729df00b72b140a3d2a2
Zack Michener <zack@zackmichener.net>
2013-06-08 13:38:25 -0700

    separate view rendering and socket logic

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,35 +1,25 @@
-window.socket;
-
 function SocketController() {
-    var socket = new io.connect(window.location.host)
+    var socket
       , otherPlayers = []
       , thisPlayer
       , game
       , gameListUpdate;
 
-    /** public members/methods */
+    socket = new io.connect(window.location.host);
+
     var _SocketController = {
 
         "joinLobby": function() {
-            $(".content").replaceWith($("#loading"));
-
-            gameListUpdate = setInterval(function() {
-                socket.emit("getGameList");
-            }, 1000);
-
-            socket.on("gameList", this.updateGameList);
+            socket.on("gameList", window.contents.updateGameList);
         },
 
-        "ready": function(numPlayers) {
-            socket.emit("ready", {
-                "players": numPlayers,
-            });
+        "getGameList": function() {
+            socket.emit("getGameList");
+        },
 
-            socket.on("wait", this.wait);
-            socket.on("go", this.go);
-            socket.on("msg", this.receive);
-            socket.on("quit", this.playerQuit);
-            socket.on("wormhole", this.wormhole);
+        "joinGame": function(name) {
+            socket.emit("joinGame", name);
+            this.waitForGame();
         },
 
         "newGame": function(game, numPlayers) {
@@ -38,14 +28,18 @@ function SocketController() {
                 "name": game,
                 "numPlayers": numPlayers,
             });
+            this.waitForGame();
         },
 
-        "wait": function() {
-            console.log("Server said wait.");
-            $("#btnPlay").replaceWith($("<button disabled='disabled' class='btn btn-large btn-primary'>Waiting...</button>"));
+        "waitForGame": function() {
+            socket.on("wait", function() {
+                console.log("Server said wait.");
+            });
+
+            socket.on("go", this.startGame);
         },
 
-        "go": function(data) {
+        "startGame": function(data) {
             console.log("Server said go.");
 
             otherPlayers = data.players;
@@ -53,13 +47,11 @@ function SocketController() {
 
             game = new Game(thisPlayer, otherPlayers);
 
-            $("#btnPlay").hide();
-            window.scrollTo(0, 0);
-            $("#wormhole").fadeIn();
+            window.contents.startGame();
 
             game.play();
         },
-
+        
         "send": function(to, obj) {
             obj = obj || {};
             socket.emit("msg", {
@@ -107,36 +99,9 @@ function SocketController() {
             });
         },
 
-        "updateGameList": function(games) {
-            $("#roomList").replaceWith("<ul id='roomList'></ul>").show();
-            for (var game in games) {
-                game = JSON.parse(games[game]);
-                // $("#roomList").append("<li class='" + game.status + "'>" + 
-                //         game.name + " (" + game.numPlayers + ")" +
-                //         "<button type='button' class='join' data-game='" + 
-                //         game.name + "'>Join</button>");
-                $("#roomList").append(Mustache.to_html($("#gameInfo").html(), game));
-            }
-        },
-
     };
 
     return _SocketController;
 
 }
 
-$(document).ready(function() {
-    window.socket = SocketController();
-
-    var waitingBtn = $("<div class='center'><button class='btn btn-large btn-primary' disabled='disabled'>Waiting for players...</button></div>");
-
-    $("#btnPlay").click(function() {
-        window.socket.joinLobby();
-
-        $("#waitingArea").show();
-        $("#btnAddRoom").click(function() {
-            window.socket.newGame($("#newRoom").val(), $("#newRoomSize").val());
-        });
-    });
-
-});
