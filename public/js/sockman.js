c053075bc0bd34a4f49a1dac1076245c528eba28
Zack Michener <zack@zackmichener.net>
2014-02-23 11:55:02 -0800

    make sockettest into full socket manager module

---
+++ public/js/sockman.js
@@ -0,0 +1,29 @@
+var Sockman = (function(Sockman) {
+  var socket = io.connect('http://localhost');
+
+  socket.on('message', function(data) {
+    console.log(data);
+  });
+
+  Sockman.ready = function(id) {
+    socket.emit('present', id);
+  };
+
+  Sockman.join = function(id) {
+    socket.emit('join', id);
+    $('#join').remove();
+  };
+
+  socket.on('playerJoined', function(playerid) {
+    console.log('Player joined: ' + playerid);
+    $('#playerlist').append('<li id="' + playerid + '">' + playerid);
+  });
+
+  socket.on('playerLeft', function(playerid) {
+    console.log('Player left: ' + playerid);
+    $('li#' + playerid).remove();
+  });
+
+  return Sockman;
+
+})({});
